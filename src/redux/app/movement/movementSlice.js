f1b5ba90f340258c0210d3738a8fcdfe2d8b7a1b
David Marino <david.marino@bayer.com>
2023-02-09 08:19:26 -0600

    asd

--- src/redux/app/movement/movementSlice.js
+++ src/redux/app/movement/movementSlice.js
@@ -11,6 +11,23 @@ const getMixedArr = (SIZE) => {
         mixedArr[i].value = mixedArr[pos].value
         mixedArr[pos].value = temp
     }
+    for (let i = 0; i < SIZE; i++) {
+        if (mixedArr[i].value === 23) {
+            const temp = mixedArr[i].value
+            mixedArr[i].value = mixedArr[23].value
+            mixedArr[23].value = temp
+        }
+        else if (mixedArr[i].value === 19) {
+            const temp = mixedArr[i].value
+            mixedArr[i].value = mixedArr[19].value
+            mixedArr[19].value = temp
+        }
+        else if (mixedArr[i].value === 18) {
+            const temp = mixedArr[i].value
+            mixedArr[i].value = mixedArr[18].value
+            mixedArr[18].value = temp
+        }
+    }
     return mixedArr
 }
 
